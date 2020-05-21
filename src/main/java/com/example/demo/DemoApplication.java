3f65c4c2ef7f8ddd9fb0b0de46ed0b26c1c24579
zhushj3 <16120039@bjtu.edu.cn>
2020-05-21 15:08:07 +0800

    pid

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,8 +1,8 @@
 package com.example.demo;
-
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 
 // 主入口，此时需要声明扫描的mapper接口（其他配置会自动扫描）
 @SpringBootApplication
@@ -10,6 +10,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        SpringApplication application = new SpringApplication(DemoApplication.class);
+        application.addListeners(new ApplicationPidFileWriter());
+        application.run(args);
     }
 }
--- src/main/java/com/example/demo/MyApplicationRunner.java
+++ src/main/java/com/example/demo/MyApplicationRunner.java
@@ -4,6 +4,8 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
+import java.lang.management.ManagementFactory;
+
 /**
  * 应用程序启动后执行，参数由DemoApplication传递过来；可以通过@order注解对该父类的子类实现进行排序执行
  * @author zhushj3
@@ -14,5 +16,7 @@ public class MyApplicationRunner implements ApplicationRunner {
     @Override
     public void run(ApplicationArguments args) throws Exception {
         System.out.println("========== 我的springboot框架启动成功 ==========");
+        String name  =  ManagementFactory.getRuntimeMXBean().getName();
+        String pid = name.split("@")[0];
     }
 }
